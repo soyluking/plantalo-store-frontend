96d65aa620b8574a2e5a7ad126149e3a74c0ddce
Luigi Pizarro <lpizarro@digitalmeat.cl>
2021-03-06 12:38:08 -0300

    feat: add layout and page componentes, styled components and router with nprogress

---
+++ components/Page.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    font-style: normal;
+    font-weight: normal;
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+  }
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offwhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.5rem;
+    line-height: 2;
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: var(--black);
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const InnerStyles = styled.div`
+  margin: 0 auto;
+  max-width: var(--maxWidth);
+  padding: 2rem;
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
---
+++ pages/sell.js
@@ -0,0 +1,3 @@
+export default function SellPage() {
+  return <h1>Sell page</h1>;
+}
