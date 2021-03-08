6cbf49b9643f7daafe103909f1ae1c53841effb9
Luigi Pizarro <lpizarro@digitalmeat.cl>
2021-03-07 21:57:52 -0300

    feat: add createProduct file input and form styles

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,42 +1,65 @@
 import useForm from '../lib/useForm';
+import FormStyles from './styles/FormStyles';
 
 export default function CreateProduct() {
-  const { inputs, handleChange, clearForm, resetForm } = useForm({
-    name: 'Amelia',
-    price: 123,
-    description: 'Planta de Amelia',
+  const { inputs, handleChange, handleSubmit } = useForm({
+    image: '',
+    name: '',
+    price: '',
+    description: '',
   });
 
   return (
-    <form>
-      <label htmlFor="name">
-        Nombre del producto:
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Ejemplo: Amelia (Bugambilia)"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Precio del producto:
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="Ejemplo: 20000"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
-      <button onClick={clearForm} type="button">
-        Clear Form
-      </button>
-      <button onClick={resetForm} type="button">
-        Reset Form
-      </button>
-    </form>
+    <FormStyles onSubmit={handleSubmit}>
+      <fieldset>
+        <label htmlFor="image">
+          Imagen:
+          <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label htmlFor="name">
+          Nombre:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Ejemplo: Amelia (Bugambilia)"
+            value={inputs.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label htmlFor="price">
+          Precio:
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Ejemplo: 20000"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Descripción:
+          <textarea
+            id="description"
+            name="description"
+            rows="8"
+            placeholder="Ejemplo: La mejor planta para interiores, no necesita demasiada agua y adorna de forma increíble..."
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">
+          Agregar Producto <i className="material-icons">eco</i>
+        </button>
+      </fieldset>
+    </FormStyles>
   );
 }
--- components/styles/FormStyles.js
+++ components/styles/FormStyles.js
@@ -12,25 +12,25 @@ const loading = keyframes`
   }
 `;
 
-const Form = styled.form`
-  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
-  background: rgba(0, 0, 0, 0.02);
-  border: 5px solid white;
-  padding: 20px;
-  font-size: 1.5rem;
-  line-height: 1.5;
+const FormStyles = styled.form`
+  box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.075);
+  font-size: 1.25rem;
   font-weight: 600;
+  line-height: 1.5;
+  margin: 50px;
+  padding: 40px;
   label {
     display: block;
-    margin-bottom: 1rem;
+    margin-bottom: 1.5rem;
   }
   input,
   textarea,
   select {
-    width: 100%;
-    padding: 0.5rem;
-    font-size: 1rem;
     border: 1px solid black;
+    font-size: 1rem;
+    margin-top: 0.5rem;
+    padding: 0.5rem;
+    width: 100%;
     &:focus {
       outline: 0;
       border-color: var(--primary);
@@ -38,13 +38,24 @@ const Form = styled.form`
   }
   button,
   input[type='submit'] {
-    width: auto;
-    background: red;
-    color: white;
+    align-items: center;
+    background: var(--primary);
     border: 0;
-    font-size: 2rem;
-    font-weight: 600;
+    color: white;
+    cursor: pointer;
+    display: inline-flex;
+    font-size: 1.25rem;
+    font-weight: 700;
     padding: 0.5rem 1.2rem;
+    width: auto;
+
+    &:hover {
+      background: var(--black);
+    }
+
+    i {
+      margin-left: 0.5rem;
+    }
   }
   fieldset {
     border: 0;
@@ -53,17 +64,20 @@ const Form = styled.form`
     &[disabled] {
       opacity: 0.5;
     }
+
     &::before {
       height: 10px;
       content: '';
       display: block;
+      margin-bottom: 1rem;
       background-image: linear-gradient(
         to right,
-        #ff3019 0%,
-        #e2b04a 50%,
-        #ff3019 100%
+        #9fc1b6 0%,
+        #e1ece7 50%,
+        #9fc1b6 100%
       );
     }
+
     &[aria-busy='true']::before {
       background-size: 50% auto;
       animation: ${loading} 0.5s linear infinite;
@@ -71,4 +85,4 @@ const Form = styled.form`
   }
 `;
 
-export default Form;
+export default FormStyles;
